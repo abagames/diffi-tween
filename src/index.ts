5e97e977ca3a8ca08e2379f67902167d4feb47c3
ABA Games <cs8k-cyu@asahi-net.or.jp>
2017-04-08 21:32:28 +0900

    Add sliders

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import * as Chart from 'chart.js';
+declare const componentHandler: any;
 
 let properties: any;
+const funcs = [
+  {
+    name: 'sqrt',
+    func: (t, a) => Math.sqrt(a * t) + 1
+  },
+  {
+    name: 'linear',
+    func: (t, a) => a * t + 1
+  },
+  {
+    name: 'pow',
+    func: (t, a) => Math.pow(a * t, 2) + 1
+  }
+];
 
 window.onload = init;
 
@@ -15,9 +30,64 @@ function init() {
 }
 
 function initChart(propertyNames: string[]) {
-  var myChart = new Chart('chart', {
+  const diffiChart = new Chart('chart', {
     type: 'line',
     data: {
+      datasets: [{
+        label: ["Speed"],
+        data: [1, 2]
+      },
+      {
+        label: ["Size"],
+        data: [1, 1.5]
+      }
+      ]
     }
   });
+  console.log((<any>diffiChart).chart.controller.getDatasetMeta(0).data[1]._model.x);
+  const slidesDom = document.getElementById('chart_sliders');
+  propertyNames.forEach(n => {
+    const span = document.createElement('span');
+    span.className = 'mdl-card__title-text';
+    span.innerText = n;
+    slidesDom.appendChild(span);
+    appendFuncRadios(slidesDom, n);
+    appendSlider(slidesDom, 'param');
+    appendSlider(slidesDom, 'saw');
+  });
+}
+
+function appendFuncRadios(parent: HTMLElement, name: string) {
+  const radios = document.createElement('div');
+  let isFirst = true;
+  funcs.forEach(f => {
+    const label = document.createElement('label');
+    label.style.margin = '5px';
+    label.className = 'mdl-radio mdl-js-radio mdl-js-ripple-effect';
+    (<any>label).for = `${name}-${f.name}`;
+    label.innerHTML = `
+      <input type="radio" id="option-1" class="mdl-radio__button" 
+      name="${name}" value="1" ${isFirst ? 'checked' : ''}>
+      <span class="mdl-radio__label">${f.name}</span>
+    `;
+    radios.appendChild(label);
+    componentHandler.upgradeElement(label);
+    isFirst = false;
+  });
+  parent.appendChild(radios);
+}
+
+function appendSlider(parent: HTMLElement, name: string) {
+  const span = document.createElement('span');
+  span.textContent = name;
+  parent.appendChild(span);
+  const slider = document.createElement('input');
+  slider.className = 'mdl-slider mdl-js-slider';
+  slider.type = 'range';
+  slider.min = '0';
+  slider.max = '1';
+  slider.value = '1';
+  slider.step = '0.1';
+  parent.appendChild(slider);
+  componentHandler.upgradeElement(slider);
 }
